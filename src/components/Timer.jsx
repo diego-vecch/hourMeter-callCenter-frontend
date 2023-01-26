e7ae45c81addf670a9c52edc490c1dce214c312a
diego-vecch <josediegoferia@gmail.com>
2023-01-26 16:54:47 -0300

    hotfix: Make the counter stop and resume

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
-
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 
-export const Timer = ({ time }) => {
-  const [startTime, setStartTime] = useState(time);
-  const [counterTime, setCounterTime] = useState(0);
-
-  useEffect(() => {
-    if (startTime !== 0) {
-      setInterval(() => setCounterTime(Date.now() - startTime), 1000);
-    }
-  }, [startTime]);
-
+export const Timer = ({ counterTime }) => {
   return (
     <div className="grid grid-cols-3 gap-0 vy-sm:gap-2">
       {Object.entries({
