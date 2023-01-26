51d6712613f31cbaed5076a18a3e87821acf5b29
diego-vecch <josediegoferia@gmail.com>
2023-01-26 17:58:52 -0300

    feat: Build Button type children

--- src/components/Working.jsx
+++ src/components/Working.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from "react";
+import Button from "./Button";
 
 import { Timer } from "./Timer";
 
@@ -36,19 +37,14 @@ export const Working = () => {
     <div className="bg-base-300 border-2 rounded border-blue-900 justify-center items-center m-2 px-2 vy-sm:mt-4 vy-sm:mx-4 vy-sm:w-auto sm:my-2 sm:mx-10 sm:px-10 md:h-60 md:mr-8 md:ml-2 md:mt-8 md:px-6 lg:h-60 lg:my-10 lg:mr-12 lg:ml-4">
       <h2 className="text-2xl mt-7 mb-8 mx-10 text-center ">Trabajando</h2>
       <Timer counterTime={counterTime} />
-      <div className="flex gap-2 mt-4">
+      <div className=" grid grid-cols-2 gap-1 mt-4 vy-sm:ml-2 md:ml-4 xl:ml-10">
         {!isResting && (
-          <button
-            onClick={handleRestClick}
-            className="bg-slate-800 w-28 h-10 text-sm"
-          >
-            Descanso
-          </button>
+          <Button onClick={handleRestClick}>Descanso</Button>
         )}
         {isResting && (
-          <button onClick={handleContinueClick} className="bg-slate-800 w-28 h-10 text-sm">Retomar</button>
+          <Button onClick={handleContinueClick}>Retomar</Button>
         )}
-        <button className="bg-slate-800 w-40 text-sm">Finalizar Jornada</button>
+        <Button>Finalizar</Button>
       </div>
     </div>
   );
